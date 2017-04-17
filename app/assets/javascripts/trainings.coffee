acf64dac8db4384a758d9363cdbdef1b0625e1b3
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2017-04-16 23:13:13 -0500

    Colaborador y área responsable

--- app/assets/javascripts/trainings.coffee
+++ app/assets/javascripts/trainings.coffee
@@ -20,3 +20,14 @@
     $('#training_category_other').removeAttr('name')
     category_select.attr('name', 'training[category]')
     $('#category_form_group').hide('slow')
+
+@attendantChanged = ->
+  attendant = $("input:radio[name ='attendant']:checked").val();
+  if attendant == 'area'
+    $('#collaborator_form_group').hide('slow')
+    $('#area_form_group').show('slow')
+    $('#training_collaborator_id').val(null)
+  else if attendant == 'collaborator'
+    $('#area_form_group').hide('slow')
+    $('#collaborator_form_group').show('slow')
+    $('#training_area_id').val(null)
--- app/models/training.rb
+++ app/models/training.rb
@@ -5,13 +5,24 @@ class Training < ApplicationRecord
   validates :topic, presence: true
   validates :date, presence: true
   validates :hourly_intensity, numericality: { greater_than_or_equal_to: 0 }
+  validate :area_and_collaborator_validator
   validate :objective_validator
+
+  def area_and_collaborator_validator
+    if area_id.blank? && collaborator_id.blank?
+      errors.add(:area_id, 'can not be blank')
+      errors.add(:collaborator_id, 'can not be blank')
+    elsif area_id.present? && collaborator_id.present?
+      errors.add(:area_id, 'can not be present')
+      errors.add(:collaborator_id, 'can not be present')
+    end
+  end
 
   def objective_validator
     if planned && objective.blank?
-      errors.add(:objective, 'can not be present if the training was planned')
+      errors.add(:objective, 'can not be blank if the training was planned')
     elsif !planned && objective.present?
-      errors.add(:objective, 'can not be blank if the training was not planned')
+      errors.add(:objective, 'can not be present if the training was not planned')
     end
   end
 
