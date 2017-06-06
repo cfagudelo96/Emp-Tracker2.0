5827951abc8a6eb5d6d7b1513f5ee84078b2820d
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2017-06-06 13:43:01 -0500

    Corrección mini

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  resources :attendances
   resources :trainings do
     collection do
-      get 'all'
+      get :all
     end
   end
   resources :training_executions do
     member do
-      get 'register_attendances'
-      patch 'save_attendances'
+      get :register_attendances
+      patch :save_attendances
     end
   end
   resources :employees
