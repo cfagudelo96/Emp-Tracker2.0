59fe1bdf7ba71ab346853aa800efe0bf165b5486
Carlos Felipe Agudelo <cf.agudelo12@uniandes.edu.co>
2017-06-14 18:56:02 -0500

    cambio a mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'emptracker2.0@gmail.com'
-  layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,13 +62,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'gmail.com',
-      user_name: ENV['MAILER_USER_NAME'],
-      password: ENV['MAILER_PASSWORD'],
-      authentication: 'plain',
-      enable_starttls_auto: true
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'emptracker2.0@gmail.com',
+    password: ENV['MAILER_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
